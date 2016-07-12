bc54588a3869fb56ae8768686dc6ae27e09fe47f
Alex-Gurung <aag1234@gmail.com>
2016-07-12 16:39:30 -0400

    Initial commit Added the files

---
+++ Convert.py
@@ -0,0 +1,12 @@
+from yahoo_finance import Share
+
+inp = open('input.txt', 'r')
+out = open('newOutput.txt', 'w')
+for line in inp:
+	temp = Share(line)
+	temp.refresh()
+	out.write(line.rstrip("\n") + "," + temp.get_price() + "," + temp.get_change() + "\n")
+	#Symbol, last closing price, last change, fund name
+#"," + temp.get_info() + 
+temp = Share('FUSVX')
+print(temp.get_stock_exchange())
